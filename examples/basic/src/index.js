ae2eafb248ad8a0d0994fec9f4d0044d88c63599
Tyrone Tudehope <tyronetudehope@gmail.com>
2018-06-09 21:39:34 +0200

    Change callback names

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,8 +1,8 @@
 const create = require('../../../')
 const userMiddleware = require('./handler')
 
-const start = create(userMiddleware)
-start(3000, server => {
+const listen = create(userMiddleware)
+listen(3000, server => {
   const { address, port } = server.address()
   console.log(`Server listening on ${address}:${port}`)
 })
